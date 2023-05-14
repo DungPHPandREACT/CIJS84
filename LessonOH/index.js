7bcc0f759e65b29298896af123da64c02964bb3a
DUNGPHPANDREACT <dung2000cn@gmail.com>
2023-05-14 20:42:29 +0700

    lesson oh - spread operator

--- LessonOH/index.js
+++ LessonOH/index.js
@@ -28,17 +28,50 @@ const tinhBinhPhuong = (n) => n ** 2;
 // };
 
 // -Template literal: ``
-const maLop = 'CIJSXYZ';
-const diaChi = 'Hà Nội';
-// const str = 'MindX ' + maLop + '\nHọc ở ' + diaChi;
-const str = `MindX ${maLop}
-Học ở ${diaChi}`;
+// const maLop = 'CIJSXYZ';
+// const diaChi = 'Hà Nội';
+// // const str = 'MindX ' + maLop + '\nHọc ở ' + diaChi;
+// const str = `MindX ${maLop}
+// Học ở ${diaChi}`;
 
-// MindXCIJS84
-// Học ở Thành Công
-console.log(str);
+// // MindXCIJS84
+// // Học ở Thành Công
+// console.log(str);
 
 // -Spread & Rest Operator
+// Một số ứng dụng với spread operator:
+
+// - Sao chép array, object
+// const num1 = [1, 2, 3];
+// const num2 = [...num1];
+// num2[2] = 33;
+// console.log(num1);
+// console.log(num2);
+// - Gộp array, object
+const nameAndAge = { name: 'Nguyễn Văn A', age: 18 };
+const gpa = { gpa: 4.0 };
+const student = {
+	...nameAndAge,
+	...gpa,
+};
+// console.log(student);
+
+// const student1 = {name: 'Nguyễn Văn A', age: 18, gpa: 3.9};
+// const student2 = {name: 'Đỗ Văn B', age: 19}
+// const student3 = {...student1, ...student2};
+// {
+//     gpa: 3.9,
+//     name: 'Đỗ Văn B', age: 19
+// }
+// console.log(student3);
+
+const arr1 = [1, 2, 3];
+const arr2 = [1, 2, 3];
+const arr3 = [...arr1, ...arr2];
+console.log(arr3);
+
+// - Thêm phần tử vào array, thêm key vào object.
+
 // -Destructuring
 // -Module
 // -Array function: map, filter, findIndex, find...
