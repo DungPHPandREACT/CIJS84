9c5e6ad9581193139cd72f5c6b699b0c9f83f14b
DUNGPHPANDREACT <dung2000cn@gmail.com>
2023-05-14 20:03:54 +0700

    lesson oh

---
+++ LessonOH/index.js
@@ -0,0 +1,36 @@
+// -Arrow function
+// const/let/var tenFunction = (parameter1, param2,...paramsN) => {
+//     // code block
+// }
+
+// Viết 1 hàm tính tổng bằng cách sử dụng arrow function
+// Input: 2,6
+// Output: 8
+const sum = (a, b) => {
+	return a + b;
+};
+
+const tinhBinhPhuong = (n) => n ** 2;
+
+// console.log(sum(2, 6));
+// console.log(tinhBinhPhuong(6));
+// Viết 1 hàm tính bình phương của 1 số n
+// Input: 6
+// Output: 36
+// *Arrow function không có hoisting
+// *Arrow function không có context
+// logger();
+// function logger() {
+// 	console.log('Xin chào thế giới');
+// }
+// const logger = () => {
+// 	console.log('Xin chào thế giới');
+// };
+
+// -Template literal
+// -Spread & Rest Operator
+// -Destructuring
+// -Module
+// -Array function: map, filter, findIndex, find...
+// -Tạo project trong reactjs
+// -Component trong reactjs
