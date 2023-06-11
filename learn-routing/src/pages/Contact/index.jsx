97b1cd6e7336751e094901f88b7a20ab6a308af3
DUNGPHPANDREACT <dung2000cn@gmail.com>
2023-06-11 22:35:42 +0700

    pagination

--- learn-routing/src/pages/Contact/index.jsx
+++ learn-routing/src/pages/Contact/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 const Contact = () => {
+	const handlePostBlog = async () => {
+		fetch('https://6485ce17a795d24810b75639.mockapi.io/api/v1/blogs/27', {
+			method: 'DELETE',
+			headers: {
+				Accept: 'application/json, text/plain, */*',
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((res) => res.json())
+			.then((res) => console.log(res));
+	};
 	return (
 		<main className='mb-4'>
 			<div className='container px-4 px-lg-5'>
@@ -11,118 +22,109 @@ const Contact = () => {
 							and I will get back to you as soon as possible!
 						</p>
 						<div className='my-5'>
-							{/* * * * * * * * * * * * * * * **/}
-							{/* * * SB Forms Contact Form * **/}
-							{/* * * * * * * * * * * * * * * **/}
-							{/* This form is pre-integrated with SB Forms.*/}
-							{/* To make this form functional, sign up at*/}
-							{/* https://startbootstrap.com/solution/contact-forms*/}
-							{/* to get an API token!*/}
-							<form id='contactForm' data-sb-form-api-token='API_TOKEN'>
-								<div className='form-floating'>
-									<input
-										className='form-control'
-										id='name'
-										type='text'
-										placeholder='Enter your name...'
-										data-sb-validations='required'
-									/>
-									<label htmlFor='name'>Name</label>
-									<div
-										className='invalid-feedback'
-										data-sb-feedback='name:required'
-									>
-										A name is required.
-									</div>
-								</div>
-								<div className='form-floating'>
-									<input
-										className='form-control'
-										id='email'
-										type='email'
-										placeholder='Enter your email...'
-										data-sb-validations='required,email'
-									/>
-									<label htmlFor='email'>Email address</label>
-									<div
-										className='invalid-feedback'
-										data-sb-feedback='email:required'
-									>
-										An email is required.
-									</div>
-									<div
-										className='invalid-feedback'
-										data-sb-feedback='email:email'
-									>
-										Email is not valid.
-									</div>
-								</div>
-								<div className='form-floating'>
-									<input
-										className='form-control'
-										id='phone'
-										type='tel'
-										placeholder='Enter your phone number...'
-										data-sb-validations='required'
-									/>
-									<label htmlFor='phone'>Phone Number</label>
-									<div
-										className='invalid-feedback'
-										data-sb-feedback='phone:required'
-									>
-										A phone number is required.
-									</div>
-								</div>
-								<div className='form-floating'>
-									<textarea
-										className='form-control'
-										id='message'
-										placeholder='Enter your message here...'
-										style={{ height: '12rem' }}
-										data-sb-validations='required'
-										defaultValue={''}
-									/>
-									<label htmlFor='message'>Message</label>
-									<div
-										className='invalid-feedback'
-										data-sb-feedback='message:required'
-									>
-										A message is required.
-									</div>
-								</div>
-								<br />
-								{/* Submit success message*/}
-								{/**/}
-								{/* This is what your users will see when the form*/}
-								{/* has successfully submitted*/}
-								<div className='d-none' id='submitSuccessMessage'>
-									<div className='text-center mb-3'>
-										<div className='fw-bolder'>Form submission successful!</div>
-										To activate this form, sign up at
-										<br />
-										<a href='https://startbootstrap.com/solution/contact-forms'>
-											https://startbootstrap.com/solution/contact-forms
-										</a>
-									</div>
-								</div>
-								{/* Submit error message*/}
-								{/**/}
-								{/* This is what your users will see when there is*/}
-								{/* an error submitting the form*/}
-								<div className='d-none' id='submitErrorMessage'>
-									<div className='text-center text-danger mb-3'>
-										Error sending message!
-									</div>
-								</div>
-								{/* Submit Button*/}
-								<button
-									className='btn btn-primary text-uppercase disabled'
-									id='submitButton'
-									type='submit'
+							<div className='form-floating'>
+								<input
+									className='form-control'
+									id='name'
+									type='text'
+									placeholder='Enter your name...'
+									data-sb-validations='required'
+								/>
+								<label htmlFor='name'>Name</label>
+								<div
+									className='invalid-feedback'
+									data-sb-feedback='name:required'
 								>
-									Send
-								</button>
-							</form>
+									A name is required.
+								</div>
+							</div>
+							<div className='form-floating'>
+								<input
+									className='form-control'
+									id='email'
+									type='email'
+									placeholder='Enter your email...'
+									data-sb-validations='required,email'
+								/>
+								<label htmlFor='email'>Email address</label>
+								<div
+									className='invalid-feedback'
+									data-sb-feedback='email:required'
+								>
+									An email is required.
+								</div>
+								<div
+									className='invalid-feedback'
+									data-sb-feedback='email:email'
+								>
+									Email is not valid.
+								</div>
+							</div>
+							<div className='form-floating'>
+								<input
+									className='form-control'
+									id='phone'
+									type='tel'
+									placeholder='Enter your phone number...'
+									data-sb-validations='required'
+								/>
+								<label htmlFor='phone'>Phone Number</label>
+								<div
+									className='invalid-feedback'
+									data-sb-feedback='phone:required'
+								>
+									A phone number is required.
+								</div>
+							</div>
+							<div className='form-floating'>
+								<textarea
+									className='form-control'
+									id='message'
+									placeholder='Enter your message here...'
+									style={{ height: '12rem' }}
+									data-sb-validations='required'
+									defaultValue={''}
+								/>
+								<label htmlFor='message'>Message</label>
+								<div
+									className='invalid-feedback'
+									data-sb-feedback='message:required'
+								>
+									A message is required.
+								</div>
+							</div>
+							<br />
+							{/* Submit success message*/}
+							{/**/}
+							{/* This is what your users will see when the form*/}
+							{/* has successfully submitted*/}
+							<div className='d-none' id='submitSuccessMessage'>
+								<div className='text-center mb-3'>
+									<div className='fw-bolder'>Form submission successful!</div>
+									To activate this form, sign up at
+									<br />
+									<a href='https://startbootstrap.com/solution/contact-forms'>
+										https://startbootstrap.com/solution/contact-forms
+									</a>
+								</div>
+							</div>
+							{/* Submit error message*/}
+							{/**/}
+							{/* This is what your users will see when there is*/}
+							{/* an error submitting the form*/}
+							<div className='d-none' id='submitErrorMessage'>
+								<div className='text-center text-danger mb-3'>
+									Error sending message!
+								</div>
+							</div>
+							{/* Submit Button*/}
+							<button
+								className='btn btn-primary text-uppercase'
+								id='submitButton'
+								onClick={handlePostBlog}
+							>
+								Send
+							</button>
 						</div>
 					</div>
 				</div>
--- learn-routing/src/pages/Home/index.jsx
+++ learn-routing/src/pages/Home/index.jsx
@@ -3,41 +3,73 @@ import Blog from './components/Blog';
 
 const Home = () => {
 	const [listBlog, setListBlog] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [page, setPage] = useState(1);
+
+	const handleSelectPage = (pg) => {
+		setPage(pg);
+	};
 
 	useEffect(() => {
-		fetch('https://6485ce17a795d24810b75639.mockapi.io/api/v1/blogs')
+		setLoading(true);
+		fetch(
+			`https://6485ce17a795d24810b75639.mockapi.io/api/v1/blogs?page=${page}&limit=10`,
+			
+		)
 			.then((response) => {
 				return response.json();
 			})
 			.then((data) => {
 				console.log(data);
 				setListBlog(data);
+				setLoading(false);
 			});
-	}, []);
+	}, [page]);
 
 	return (
 		<div className='container px-4 px-lg-5'>
-			<div className='row gx-4 gx-lg-5 justify-content-center'>
-				<div className='col-md-10 col-lg-8 col-xl-7'>
-					{listBlog.map((blog) => (
-						<Blog
-							key={blog.id}
-							id={blog.id}
-							title={blog.title}
-							description={blog.description}
-							author={blog.author}
-							createdAt={blog.createdAt}
-						/>
-					))}
+			{loading === true ? (
+				<div className='spinner-border' role='status'>
+					<span className='visually-hidden'>Loading...</span>
+				</div>
+			) : (
+				<div className='row gx-4 gx-lg-5 justify-content-center'>
+					<div className='col-md-10 col-lg-8 col-xl-7'>
+						{listBlog.map((blog) => (
+							<Blog
+								key={blog.id}
+								id={blog.id}
+								title={blog.title}
+								description={blog.description}
+								author={blog.author}
+								createdAt={blog.createdAt}
+							/>
+						))}
 
-					{/* Pager*/}
-					<div className='d-flex justify-content-end mb-4'>
-						<a className='btn btn-primary text-uppercase' href='#!'>
-							Older Posts →
-						</a>
+						{/* Pager*/}
+						<ul className='pagination'>
+							<li className='page-item disabled'>
+								<a className='page-link'>Previous</a>
+							</li>
+							{[1, 2, 3].map((pg) => (
+								<li
+									className={`page-item ${pg === page ? 'active' : ''}`}
+									onClick={() => handleSelectPage(pg)}
+								>
+									<a className='page-link' href='#'>
+										{pg}
+									</a>
+								</li>
+							))}
+							<li className='page-item'>
+								<a className='page-link' href='#'>
+									Next
+								</a>
+							</li>
+						</ul>
 					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 };
